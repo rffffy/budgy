7db09a77f92dda805d325ca9a5419b66224a5a8a
rffffy <raafay.alam@gmail.com>
2019-09-28 03:07:36 +0200

    interface change

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { ReactComponent as Logo } from "../../assets/Budgy-logo-3/vector/default-monochrome.svg";
+// import { ReactComponent as Logo } from "../../assets/Budgy-logo-3/vector/default-monochrome.svg";
+import { ReactComponent as Logo } from "../../assets/Budgy-logo-4/vector/default-monochrome.svg";
 // import { ReactComponent as LogoBlack } from "../../assets/Budgy-logo-3/vector/default-monochrome-black.svg";
 // import { ReactComponent as LogoWhite } from "../../assets/Budgy-logo-3/vector/default-monochrome-white.svg";
 // import { ReactComponent as LogoAlt } from "../../assets/Budgy-logo/vector/default-monochrome.svg";
